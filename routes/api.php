acff56bf9cb8bee2912c24507e73e70f67836e73
Ayouub-aj <Aigle313@gmail.com>
2026-08-01 18:30:35 +0100

    job offers CRUD module (model, factory, policy, resource, controller

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/job-offers', [JobOfferController::class, 'index']);
-Route::get('/job-offers/{id}', [JobOfferController::class, 'show']);
+Route::get('/job-offers/{jobOffer}', [JobOfferController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
